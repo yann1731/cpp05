97f6e623cb6c1cd81b8f093be813ff55f7b7d917
yannick st-laurent <yst-laur@student.42quebec.com>
2022-12-19 13:08:02 -0500

    Implementing Form class

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -1,6 +1,46 @@
 #include "Form.hpp"
 
+class Form
+{
+	public:
+	Form();
+	Form(const std::string name, const int gradeSign, const int gradeExec);
+	~Form();
+	const std::string getName(void) const;
+	const bool getSignstatus(void) const;
+	const int getGradeSign(void) const;
+	const int getGradeExec(void) const;
+	class GradeTooLowException: public std::exception
+    {
+        virtual const char *what() const throw();
+    };
+    class GradeTooHighException: public std::exception
+    {
+    	const char *what() const throw();
+    };
+	private:
+	const std::string _name;
+	bool _isSigned;
+	const int _gradeSign;
+	const int _gradeExec;
+};
 
+Form::Form(): _name("Random form"), _gradeExec(1), _gradeSign(1), _isSigned(false)
+{
+	return ;
+}
+
+Form::Form(const std::string name, const int gradeSign, const int gradeExec): _name(name), _gradeSign(gradeSign), _gradeExec(gradeExec), _isSigned(false)
+{
+	if (gradeSign < 1)
+		throw (GradeTooHighException());
+	if (gradeSign > 150)
+		throw (GradeTooLowException());
+	if (gradeExec < 1)
+		throw (GradeTooHighException());
+	if (gradeExec > 150)
+		throw (GradeTooLowException());	
+}
 
 
 
